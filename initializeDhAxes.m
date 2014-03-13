a1d3bd701bfc11bca68edb5cbb13b4c2b2917c25
Dave Fisher <fisherds@rose-hulman.edu>
2014-03-13 11:02:27 -0400

    Adding comments

--- initializeDhAxes.m
+++ initializeDhAxes.m
@@ -1,9 +1,13 @@
 function initializeDhAxes
 
+% Begin: Setup for this stand alone version that will not go into the GUI.
 close all
-clc
 handles.axes_arm = axes;
 handles.user.jointAngles = [0 90 0 -90 90]; % Home position.
+% End: Setup for this stand alone version that will not go into the GUI.
+
+% Begin: Code that can go into your GUI's opening function.
+clc
 
 % Prepare the arm axes
 view(handles.axes_arm, [-50 -50 50]);
@@ -14,7 +18,7 @@ ylabel('y')
 zlabel('z')
 
 % Create vertices for all the patches
-makeLink0(handles.axes_arm, [.5 .5 .5]);  % Doesn't move so save no references.
+makeLink0(handles.axes_arm, [.5 .5 .5]);  % Doesn't move. No handles needed.
 % Save handles to the patch objects and create a vertices matrix for each.
 handles.user.link1Patch = makeLink1(handles.axes_arm, [.9 .9 .9]);
 handles.user.link1Vertices = get(handles.user.link1Patch, 'Vertices')';
@@ -31,6 +35,7 @@ handles.user.link4Vertices(4,:) = ones(1, size(handles.user.link4Vertices,2));
 handles.user.link5Patch = makeLink5(handles.axes_arm, [.95 .95 0]);
 handles.user.link5Vertices = get(handles.user.link5Patch, 'Vertices')';
 handles.user.link5Vertices(4,:) = ones(1, size(handles.user.link5Vertices,2));
+% End: Code that can go into your GUI's opening function.
 
 updateArm('unused', handles);
 
