f9270640ed2dc98db02d50040ceb7098d8206eda
Dave Fisher <fisherds@rose-hulman.edu>
2014-03-13 11:40:05 -0400

    Renamed A matrices

--- makeHomogeneousTransformations.m
+++ makeHomogeneousTransformations.m
@@ -1,10 +1,10 @@
-function [T1, T2, T3, T4, T5] = makeHomogeneousTransformations(degreesTheta1, degreesTheta2, degreesTheta3, degreesTheta4, degreesTheta5)
+function [A1, A2, A3, A4, A5] = makeHomogeneousTransformations(degreesTheta1, degreesTheta2, degreesTheta3, degreesTheta4, degreesTheta5)
 %MAKEHOMOGENEOUSTRANSFORMATIONS Create the DH matrices for the arm.
 
-% T1 = homogeneousTransformation(___, ___, ___, degreesTheta1);
-% T2 = homogeneousTransformation(___, ___, ___, degreesTheta2);
-% T3 = homogeneousTransformation(___, ___, ___, degreesTheta3);
-% T4 = homogeneousTransformation(___, ___, ___, degreesTheta4);
-% T5 = homogeneousTransformation(___, ___, ___, degreesTheta5);
+% A1 = homogeneousTransformation(___, ___, ___, degreesTheta1);
+% A2 = homogeneousTransformation(___, ___, ___, degreesTheta2);
+% A3 = homogeneousTransformation(___, ___, ___, degreesTheta3);
+% A4 = homogeneousTransformation(___, ___, ___, degreesTheta4);
+% A5 = homogeneousTransformation(___, ___, ___, degreesTheta5);
 
 end
