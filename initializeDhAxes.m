7f1c72987a195508895746db781df0d5f3433f5f
David Fisher <fisherds@rose-hulman.edu>
2020-03-26 13:12:57 -0400

    update for App Designer

--- initializeDhAxes.m
+++ initializeDhAxes.m
@@ -2,9 +2,9 @@ function initializeDhAxes
 
 % Begin: Setup for this stand alone version that will not go into the GUI.
 close all
-handles.axes_arm = axes;
-%handles.user.jointAngles = [0 0 0 0 0]; % Zero angle position.
-handles.user.jointAngles = [0 90 0 -90 90]; % Servo home position.
+app.axes_arm = axes;
+%app.jointAngles = [0 0 0 0 0]; % Zero angle position.
+app.jointAngles = [0 90 0 -90 90]; % Servo home position.
 %  End : Setup for this stand alone version that will not go into the GUI.
 
 
@@ -13,41 +13,39 @@ handles.user.jointAngles = [0 90 0 -90 90]; % Servo home position.
 clc
 
 % Prepare the arm axes
-view(handles.axes_arm, [-50 -50 50]);
-axis(handles.axes_arm, [-13 10 -8 8 -3 14.5]);
-grid on
-xlabel('x')
-ylabel('y')
-zlabel('z')
+view(handles.axes_arm, [-5 -5 5]);
+axis(app.axes_arm, [-12 12 -8 8 -6 16]);
+set(app.axes_arm, 'Visible', 'off');
 
 % Create vertices for all the patches
-makeLink0(handles.axes_arm, [.5 .5 .5]);  % Doesn't move. No handles needed.
-% Save handles to the patch objects.
+makeLink0(handles.axes_arm, [.5 .5 .5]);  % Doesn't move. No need to save.
 % Save references to the vertices of each patch, make points 4x1 not 3x1.
-handles.user.link1Patch = makeLink1(handles.axes_arm, [.9 .9 .9]);
-handles.user.link1Vertices = get(handles.user.link1Patch, 'Vertices')';
-handles.user.link1Vertices(4,:) = ones(1, size(handles.user.link1Vertices,2));
-handles.user.link2Patch = makeLink2(handles.axes_arm, [.9 .9 .9]);
-handles.user.link2Vertices = get(handles.user.link2Patch, 'Vertices')';
-handles.user.link2Vertices(4,:) = ones(1, size(handles.user.link2Vertices,2));
-handles.user.link3Patch = makeLink3(handles.axes_arm, [.9 .9 .9]);
-handles.user.link3Vertices = get(handles.user.link3Patch, 'Vertices')';
-handles.user.link3Vertices(4,:) = ones(1, size(handles.user.link3Vertices,2));
-handles.user.link4Patch = makeLink4(handles.axes_arm, [.9 .9 .9]);
-handles.user.link4Vertices = get(handles.user.link4Patch, 'Vertices')';
-handles.user.link4Vertices(4,:) = ones(1, size(handles.user.link4Vertices,2));
-handles.user.link5Patch = makeLink5(handles.axes_arm, [.95 .95 0]);
-handles.user.link5Vertices = get(handles.user.link5Patch, 'Vertices')';
-handles.user.link5Vertices(4,:) = ones(1, size(handles.user.link5Vertices,2));
+app.link1Patch = makeLink1(handles.axes_arm, [.9 .9 .9]);
+app.link1Vertices = get(app.link1Patch, 'Vertices')';
+app.link1Vertices(4,:) = ones(1, size(app.link1Vertices,2));
+
+app.link2Patch = makeLink2(handles.axes_arm, [.9 .9 .9]);
+app.link2Vertices = get(app.link2Patch, 'Vertices')';
+app.link2Vertices(4,:) = ones(1, size(app.link2Vertices,2));
+
+app.link3Patch = makeLink3(handles.axes_arm, [.9 .9 .9]);
+app.link3Vertices = get(app.link3Patch, 'Vertices')';
+app.link3Vertices(4,:) = ones(1, size(app.link3Vertices,2));
+
+app.link4Patch = makeLink4(handles.axes_arm, [.9 .9 .9]);
+app.link4Vertices = get(app.link4Patch, 'Vertices')';
+app.link4Vertices(4,:) = ones(1, size(app.link4Vertices,2));
+
+app.link5Patch = makeLink5(handles.axes_arm, [.95 .95 0]);
+app.link5Vertices = get(app.link5Patch, 'Vertices')';
+app.link5Vertices(4,:) = ones(1, size(app.link5Vertices,2));
 % End: Code that can go into your GUI's opening function.
 
-updateArm('unused', handles);
+updateArm(app);
 
 end
 
-function updateArm(hObject, handles)
-
-% TODO: Make sure the handles.user.jointAngles values are set.
+function updateArm(app)
 
 % TODO: Create the five homogeneous transformation matrices.
 
@@ -59,24 +57,24 @@ function updateArm(hObject, handles)
 % T0_5 = 
 
 % TODO: Use the T matricies to transform the patch vertices
-% link1verticesWRTground = T0_1 * handles.user.link1Vertices;
-% link2verticesWRTground = T0_2 * handles.user.link2Vertices;
-% link3verticesWRTground = T0_3 * handles.user.link3Vertices;
-% link4verticesWRTground = T0_4 * handles.user.link4Vertices;
-% link5verticesWRTground = T0_5 * handles.user.link5Vertices;
+% link1verticesWRTground = T0_1 * app.link1Vertices;
+% link2verticesWRTground = T0_2 * app.link2Vertices;
+% link3verticesWRTground = T0_3 * app.link3Vertices;
+% link4verticesWRTground = T0_4 * app.link4Vertices;
+% link5verticesWRTground = T0_5 * app.link5Vertices;
 
 % TODO: Update the patches with the new vertices
-% set(handles.user.link1Patch,'Vertices', link1verticesWRTground(1:3,:)');
-% set(handles.user.link2Patch,'Vertices', link2verticesWRTground(1:3,:)');
-% set(handles.user.link3Patch,'Vertices', link3verticesWRTground(1:3,:)');
-% set(handles.user.link4Patch,'Vertices', link4verticesWRTground(1:3,:)');
-% set(handles.user.link5Patch,'Vertices', link5verticesWRTground(1:3,:)');
+% set(app.link1Patch,'Vertices', link1verticesWRTground(1:3,:)');
+% set(app.link2Patch,'Vertices', link2verticesWRTground(1:3,:)');
+% set(app.link3Patch,'Vertices', link3verticesWRTground(1:3,:)');
+% set(app.link4Patch,'Vertices', link4verticesWRTground(1:3,:)');
+% set(app.link5Patch,'Vertices', link5verticesWRTground(1:3,:)');
 
 
-% Optional code (if you want to display the XYZ of the gripper).
+% Display the XYZ of the gripper tip.
 % Update x, y, and z using the gripper (end effector) origin.
 % dhOrigin = [0 0 0 1]';
 % gripperWRTground = T0_5 * dhOrigin;
-% fprintf('(%.3f, %.3f, %.3f)\n', gripperWRTground(1), gripperWRTground(2), gripperWRTground(3));
+% fprintf('TODO: Put each individual value into it's own GUI label X = %.3f, Y = %.3f, Z = %.3f)\n', gripperWRTground(1), gripperWRTground(2), gripperWRTground(3));
 
 end
