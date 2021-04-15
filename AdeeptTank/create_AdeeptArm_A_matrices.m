332e10f7bd284b10e0b5c59b5ca6687e391b3213
David Fisher <fisherds@rose-hulman.edu>
2021-04-15 08:52:29 -0400

    function rename

--- AdeeptTank/create_AdeeptArm_A_matrices.m
+++ AdeeptTank/create_AdeeptArm_A_matrices.m
@@ -1,8 +1,8 @@
-function [A1, A2, A3] = create_AdeeptArm_A_matrices(degreesTheta1, degreesTheta2, degreesTheta3)
-%MAKEHOMOGENEOUSTRANSFORMATIONS Create the DH matrices for the arm.
+function [A1, A2, A3] = create_AdeeptArm_A_matrices(jointAngles)
+% Create the A matrices for the arm.
 
-% A1 = homogeneousTransformation(___, ___, ___, degreesTheta1);
-% A2 = homogeneousTransformation(___, ___, ___, degreesTheta2);
-% A3 = homogeneousTransformation(___, ___, ___, degreesTheta3);
+% A1 = create_A_matrix(___, ___, ___, jointAngles(1));
+% A2 = create_A_matrix(___, ___, ___, jointAngles(2));
+% A3 = create_A_matrix(___, ___, ___, jointAngles(3));
 
 end
